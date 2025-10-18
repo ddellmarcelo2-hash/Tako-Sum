a816e2d47ae2023946a92d7f4ec783ab17ac0d89
Ddell <ddellmarcelo2@gmail.com>
2025-10-18 22:12:21 +0800

    Co-authored-by: cryptojack102294 <cryptojack102294@users.noreply.github.com>

--- takosum.js
+++ takosum.js
@@ -1,7 +1,7 @@
 let menuButton = document.getElementById('menu-button');
 let navlinkWrapper = document.getElementById('nav-link');
 
-menuButton.addEventListener('click' , () => {
+menuButton.addEventListener('click', () => {
     navlinkWrapper.classList.toggle('show');
 });
 
@@ -10,20 +10,22 @@ let heroContent = document.querySelector('.hero-paragraph');
 
 home.addEventListener('click', () => {
     heroContent.classList.add('animate');
-    setTimeout(()=>{
-    heroContent.classList.remove('animate');
-},1000);
-    
+    setTimeout(() => {
+        heroContent.classList.remove('animate');
+    }, 1000);
+
 });
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
         }
     });
 });
-let images = document.querySelectorAll('img');
+let images = document.querySelectorAll('p');
 let productCards = document.querySelectorAll('.products-card');
 productCards.forEach(productCard => {
     observer.observe(productCard);
@@ -32,3 +34,46 @@ images.forEach(image => {
     observer.observe(image);
 });
 
+
+var swiper = new Swiper(".takoyaki-swiper", {
+    spaceBetween: 40,
+    loop: 'true',
+    autoplay: 'true',
+    slidesPerView: 3,
+    navigation: {
+        nextEl: "#takoyaki-next",
+        prevEl: "#takoyaki-prev",
+    },
+    pagination: {
+        el: "#takoyaki-pagination",
+        clickable: 'true',
+    },
+});
+var swiper = new Swiper(".dimsum-swiper", {
+    spaceBetween: 40,
+    loop: 'true',
+    autoplay: 'true',
+    slidesPerView: 3,
+    navigation: {
+        nextEl: "#dimsum-next",
+        prevEl: "#dimsum-prev",
+    },
+    pagination: {
+        el: "#dimsum-pagination",
+        clickable: 'true',
+    },
+});
+var swiper = new Swiper(".rice-meal-swiper", {
+    spaceBetween: 40,
+    loop: 'true',
+    autoplay: 'true',
+    slidesPerView: 3,
+    navigation: {
+        nextEl: "#rice-meal-next",
+        prevEl: "#rice-meal-prev",
+    },
+    pagination: {
+        el: "#ricemeal-pagination",
+        clickable: 'true',
+    },
+});
